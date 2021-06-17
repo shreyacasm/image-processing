de23a34f0535a2ffe28b93c25069f08b4dfa94d4
shreyacasm <suroliya.shreya2014@gmail.com>
2021-06-17 15:14:06 +0530

    table detect

---
+++ findtable.py
@@ -0,0 +1,57 @@
+import cv2
+import pytesseract
+from PIL import Image
+import imutils
+
+
+if __name__ == '__main__':
+    # image read and data without any operation
+    image = cv2.imread('invoice/testc.png')
+    data = pytesseract.image_to_string(image, lang='eng', config='--psm 6')
+    print(data)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    # Remove vertical lines
+    vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 15))
+    detect_vertical = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, vertical_kernel, iterations=2)
+    cnts = cv2.findContours(detect_vertical, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    for c in cnts:
+        cv2.drawContours(thresh, [c], -1, (0, 0, 0), 3)
+    # Remove horizontal lines
+    horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (50, 1))
+    detect_horizontal = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, horizontal_kernel, iterations=2)
+    cnts = cv2.findContours(detect_horizontal, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    for c in cnts:
+        cv2.drawContours(thresh, [c], -1, (0, 0, 0), 2)
+
+
+
+    # Dilate to connect text and remove dots
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (15, 1))
+    dilate = cv2.dilate(thresh, kernel, iterations=2)
+    cnts = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    for c in cnts:
+        area = cv2.contourArea(c)
+        if area < 500:
+            cv2.drawContours(dilate, [c], -1, (0, 0, 0), -1)
+
+    # Bitwise-and to reconstruct image
+    result = cv2.bitwise_and(image, image, mask=dilate)
+    result[dilate == 0] = (255, 255, 255)
+
+    # OCR
+    print("**********************************************************************************")
+    data = pytesseract.image_to_string(result, lang='eng', config='--psm 6')
+    print(data)
+
+    cv2.imshow('thresh', thresh)
+    cv2.imwrite('result/invoice/thresh.png', thresh)
+    cv2.imshow('dilate', dilate)
+    cv2.imwrite('result/invoice/dilate.png', dilate)
+    cv2.imshow('result', result)
+    cv2.imwrite('result/invoice/result.png', result)
+
+    cv2.waitKey()
