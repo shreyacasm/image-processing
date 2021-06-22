d792e3236403abb48b21a65dfd713237bceaf4a9
shreyacasm <suroliya.shreya2014@gmail.com>
2021-06-23 01:38:19 +0530

    invoice changes

--- table2.py
+++ table2.py
@@ -1,14 +1,19 @@
 import cv2
 import pytesseract
+import pkg_resources
 
 
 if __name__ == '__main__':
-    image = cv2.imread('invoice/padding-names5.png')
+    image = cv2.imread('invoice/test5.jpg')
     result = image.copy()
     gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
     thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-    data = pytesseract.image_to_string(thresh, lang='eng', config='--psm 6')
-    print("original :", data)
+
+    print("tesseract version :", pytesseract.get_tesseract_version())
+    print("pytesseract version :",pkg_resources.working_set.by_key['pytesseract'].version)
+
+    data = pytesseract.image_to_string(image)
+    print("original :\n", data)
 
     # Remove horizontal lines
     horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (40,1))
@@ -35,5 +40,6 @@ if __name__ == '__main__':
     # cv2.imshow('final-result', result)
     # cv2.imwrite('result/invoice/result-fin.png', result)
     data = pytesseract.image_to_string(result, lang='eng', config='--psm 6')
-    print("After", data)
+    print("*"*100)
+    print("After:\n", data)
     cv2.waitKey()
